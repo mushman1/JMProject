3b06afaa74a965110fd0f5cfa2a24b93fb150760
mushman1 <32391007+mushman1@users.noreply.github.com>
2018-01-05 10:23:27 -0500

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author jamesmusacchio
+ *
+ */
+import java.io.*;
+public class Project1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello again");
+
+	}
+
+	
+}
